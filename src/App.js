25fa27c7a29c28f9c394d39ba608d88d13572b27
Yihang Ho <hoyihang5@gmail.com>
2016-09-03 15:02:05 +0800

    Awesome V1

--- src/App.js
+++ src/App.js
@@ -1,20 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { parser, execute } from 'bfvm';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      code: "",
+      stdout: ""
+    };
+
+    ["onCodeChange", "onRunCode"].forEach(fn => {
+      this[fn] = this[fn].bind(this);
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <textarea value={ this.state.code }
+                  onChange={ this.onCodeChange } />
+
+        <button onClick={ this.onRunCode } >Run</button>
+        <div>{ this.state.stdout }</div>
       </div>
     );
+  }
+
+  onCodeChange(event) {
+    this.setState({
+      code: event.target.value
+    });
+  }
+
+  onRunCode(event) {
+    event.preventDefault();
+
+    const ast = parser(this.state.code);
+    this.setState({
+      stdout: execute(ast)
+    });
   }
 }
 
