9853f56ac0a14d0d8abaeb66044092d68a0d0ffa
Yihang Ho <hoyihang5@gmail.com>
2016-09-04 03:16:02 +0800

    Delete file

--- src/store/reducers/files.js
+++ src/store/reducers/files.js
@@ -1,4 +1,4 @@
-import { LOAD_FILES_LIST, SET_FILES_LIST, ADD_FILE } from '../actionTypes';
+import { LOAD_FILES_LIST, SET_FILES_LIST, ADD_FILE, DELETE_FILE } from '../actionTypes';
 
 function files(state = [], action) {
   switch (action.type) {
@@ -9,6 +9,8 @@ function files(state = [], action) {
         ...state,
         action.file
       ];
+    case DELETE_FILE:
+      return state.filter(({id}) => id !== action.id);
     case LOAD_FILES_LIST:
     default:
       return state;
